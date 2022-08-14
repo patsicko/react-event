bf98e28a040f68d73b6eba9f6c3ea6fb0b7cf0d4
patsicko <patsicko@g>
2022-08-14 12:34:39 +0200

    Feat: Class component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Movies from "./components/movies";
+
+
+function App() {
+  return (
+    <>
+    <Movies/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/movies.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import styles from "./movies.module.css";
+import { getMovies } from '../fakeMovies';
+
+
+class Movies extends Component{
+    state={
+      movies:getMovies()
+    }
+
+handleDelete=(movie)=>{
+    console.log(movie)
+     const movy=this.state.movies.filter(item=>item._id !==movie._id);
+     this.setState({movies:movy})
+        
+  
+   
+   
+}
+
+
+
+    render(){
+        return(
+
+        <>
+     {console.log(this.state.movies)}
+
+   
+
+
+<table  className="table">
+        <thead>
+            <tr>
+           <th>Title</th>
+           <th>Genre</th>
+           <th>Stock</th>
+           <th>Rate</th>
+           <th></th>
+            </tr>
+        </thead>
+      <tbody>
+      {this.state.movies.map(movie=>(
+      <tr key={movie._id}>
+     
+        <td>{movie.title}</td>
+        <td>{movie.genre.name}</td>
+        <td>{movie.numberInStock}</td>
+        <td>{movie.dailyRentalRate}</td>
+         <td><button onClick={()=>this.handleDelete(movie)} className='btn btn-danger btn-sm'>Delete</button></td>
+       
+      </tr>
+      ))}
+      </tbody>
+     </table>
+
+        </>
+
+        );
+    }
+}
+
+export default Movies;
+
